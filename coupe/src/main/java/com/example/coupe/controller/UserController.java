803c9351a93b23c2ec99c821cceecec5fb3b7e09
bahREEE <bhrmitn@gmail.com>
2021-02-11 20:28:08 +0100

    aya 3ad

--- coupe/src/main/java/com/example/coupe/controller/UserController.java
+++ coupe/src/main/java/com/example/coupe/controller/UserController.java
@@ -7,6 +7,8 @@ import com.example.coupe.entities.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,4 +29,8 @@ public class UserController {
     }
 
 
+    @PostMapping(value="/saveUser")
+    public User save(@RequestBody User s){
+        return userRepository.save(s);
+    }
 }
