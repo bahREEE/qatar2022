556768c44c20e334a3a96499b077e75ec09b260f
bahREEE <bhrmitn@gmail.com>
2021-02-11 22:13:24 +0100

    3ak3ek

--- coupe/src/main/java/com/example/coupe/controller/UserController.java
+++ coupe/src/main/java/com/example/coupe/controller/UserController.java
@@ -6,8 +6,13 @@ import com.example.coupe.dao.UserRepository;
 import com.example.coupe.entities.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,9 +33,20 @@ public class UserController {
         return userRepository.findAll();
     }
 
+    @GetMapping(value = "/user/{id}")
+    public User getAuser(@PathVariable Long id){
+        return userRepository.findById(id).orElseThrow();
+    }
 
     @PostMapping(value="/saveUser")
-    public User save(@RequestBody User s){
-        return userRepository.save(s);
+    public ResponseEntity<String> save(@Validated @RequestBody User s){
+        userRepository.save(s);
+        return new ResponseEntity<>("User saved successfully !", HttpStatus.OK);
+    }
+
+    @PutMapping(value="/deleteAllusers")
+    public ResponseEntity<String> deleteUsers(){
+        userRepository.deleteAll();
+        return new ResponseEntity<>("All users deleted successfully !", HttpStatus.OK);
     }
 }
