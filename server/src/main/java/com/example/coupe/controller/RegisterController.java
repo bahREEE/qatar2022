a19a71fb2005be6da6a0f7972fb654bc33e8a653
bahREEE <bhrmitn@gmail.com>
2021-02-28 16:43:49 +0100

    fixed an error

--- server/src/main/java/com/example/coupe/controller/RegisterController.java
+++ server/src/main/java/com/example/coupe/controller/RegisterController.java
@@ -4,10 +4,8 @@ import java.time.Instant;
 
 import com.example.coupe.Services.AccountService;
 import com.example.coupe.dto.RegisterRequest;
-import com.example.coupe.entities.MyUser;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
